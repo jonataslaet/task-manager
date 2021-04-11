b9f25a3cc7865eb3d26b141267edb214fca1145d
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-04-11 15:26:29 -0300

    Incluído o service de tarefa, com os métodos básicos

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TarefaService } from './tarefa.service';
 
 
 
@@ -7,6 +8,16 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    TarefaService
   ]
 })
-export class TarefasModule { }
+export class TarefasModule {
+
+  constructor(){
+
+  }
+
+  
+}
