fde0f1b6b85c96b339294267254d9c5799195c9a
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-04-11 17:25:44 -0300

    Criado o componente de listagem de tarefas

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TarefaService } from './tarefa.service';
+import { ListarTarefaComponent } from './listar/listar-tarefa.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ListarTarefaComponent],
   imports: [
     CommonModule
   ],
